636032ee78b9365ba31e148908c723b1444be0a1
Vidia <anindhitavidia@gmail.com>
2018-03-31 13:39:12 -0400

    push basic game

---
+++ Assets/ClickManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This is the library for the Phidget
+// using Phidgets22;
+// using Phidgets22.Events;
+
+public class ClickManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+			RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+			if (hit.collider != null) {
+				Debug.Log(hit.collider.gameObject.name);
+				hit.collider.attachedRigidbody.AddForce(Vector2.up);
+				Destroy (hit.collider.gameObject);
+			}
+		}
+	}
+}
